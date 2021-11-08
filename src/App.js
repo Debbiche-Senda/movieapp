3a1cba59d8d757ab45eafbf4dbf3d418332460d2
Debbiche-Senda <debbiche.senda.douda@gmail.com>
2021-11-08 11:04:42 +0100

    checkpoint react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { data } from "./data";
 import MovieList from "./components/MovieList/MovieList";
 import NavBar from "./components/NavBar/NavBar";
+import  {Switch , Route} from "react-router-dom";
 // import AddMovie from "./components/AddMovie/AddMovie";
 // import Search from "./components/Filter/Search";
-
-
 
 function App() {
   const [movies, setMovies] = useState(data);
@@ -16,14 +15,23 @@ function App() {
   function getNewMovie(t) {
     setMovies(t);
   }
-  
 
   return (
-  
     <div className="App">
-      {  console.log(rate)}
-      <NavBar setMovies={getNewMovie} movies={movies} setFilterSearch={setFilterSearch} setRate={setRate}/>
-      <MovieList movies={movies} filterSearch={filterSearch} rate={rate}/>
+      <Switch>
+      <Route
+        path="/"
+        render={() => 
+          <NavBar
+            setMovies={getNewMovie}
+            movies={movies}
+            setFilterSearch={setFilterSearch}
+            setRate={setRate}
+          />
+        }
+      />
+      <MovieList movies={movies} filterSearch={filterSearch} rate={rate} />
+      </Switch>
     </div>
   );
 }
